62857b5f744f0ccbd7cc8fcb5c322ff07a81c322
Rodrigo77 <rodrigo2007barreto@gmail.com>
2026-09-21 18:36:47 -0300

    Aluno 2: adiciona estoque minimo e categoria

--- estoque.h
+++ estoque.h
@@ -2,11 +2,13 @@
 #define ESTOQUE_H
 
 #define MAX_ITENS 50
+#define ESTOQUE_MINIMO 5
 #define TAXA_PADRAO 0.05
 
 typedef struct Produto {
     int id;
     char codigo_barras[20];
+    char categoria[20];
     char nome[50];
     int quantidade;
     float preco;
