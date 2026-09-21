e24991b71fffd26bb7e4eb9b5d894fcdeaeb1b14
Rodrigo77 <rodrigo2007barreto@gmail.com>
2026-09-21 18:35:34 -0300

    Adiciona cabecalho base do sistema de estoque

--- estoque.h
+++ estoque.h
@@ -1,1 +1,18 @@
+#ifndef ESTOQUE_H
+#define ESTOQUE_H
 
+#define MAX_ITENS 10
+#define TAXA_PADRAO 0.05
+
+typedef struct Produto {
+    int id;
+    char nome[50];
+    int quantidade;
+    float preco;
+} Produto;
+
+void listar_produtos(Produto estoque[], int total_itens);
+float calcular_total(Produto estoque[], int total_itens);
+void exibir_menu(void);
+
+#endif
